27367f9fb2efd3f8a39d8ff60207d9bc4a5faff8
gantulgarus <gantulgarus@gmail.com>
2024-08-09 01:33:22 +0800

    some udpate

--- resources/views/schemas/create.blade.php
+++ resources/views/schemas/create.blade.php
@@ -3,27 +3,34 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <h1>Шинэ схем бүртгэх</h1>
-        <form action="{{ route('schemas.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group">
-                <label for="name">Нэр</label>
-                <input type="text" class="form-control" id="name" name="name" required>
+    <div class="container mt-2">
+        <div class="card">
+            <div class="card-header">
+                Шинэ схем бүртгэх
             </div>
-            <div class="form-group">
-                <label for="station_id">Дэд атснц</label>
-                <select class="form-control" id="station_id" name="station_id" required>
-                    @foreach ($stations as $station)
-                        <option value="{{ $station->id }}">{{ $station->name }}</option>
-                    @endforeach
-                </select>
+            <div class="card-body">
+                <form action="{{ route('schemas.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Нэр</label>
+                        <input type="text" class="form-control" id="name" name="name" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="station_id">Дэд атснц</label>
+                        <select class="form-control" id="station_id" name="station_id" required>
+                            @foreach ($stations as $station)
+                                <option value="{{ $station->id }}">{{ $station->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="image">Зураг</label>
+                        <input type="file" class="form-control" id="image" name="image">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Хадгалах</button>
+                </form>
             </div>
-            <div class="form-group">
-                <label for="image">Зураг</label>
-                <input type="file" class="form-control-file" id="image" name="image">
-            </div>
-            <button type="submit" class="btn btn-primary">Хадгалах</button>
-        </form>
+        </div>
+        
     </div>
 @endsection
--- resources/views/schemas/edit.blade.php
+++ resources/views/schemas/edit.blade.php
@@ -3,33 +3,40 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <h1>Схем засах</h1>
-        <form action="{{ route('schemas.update', $schema->id) }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-            <div class="form-group">
-                <label for="name">Нэр</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ $schema->name }}" required>
+    <div class="container mt-2">
+        <div class="card">
+            <div class="card-header">
+                Шинэ схем бүртгэх
             </div>
-            <div class="form-group">
-                <label for="station_id">Дэд станц</label>
-                <select class="form-control" id="station_id" name="station_id" required>
-                    @foreach ($stations as $station)
-                        <option value="{{ $station->id }}" {{ $schema->station_id == $station->id ? 'selected' : '' }}>
-                            {{ $station->name }}
-                        </option>
-                    @endforeach
-                </select>
+            <div class="card-body">
+                <form action="{{ route('schemas.update', $schema->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group">
+                        <label for="name">Нэр</label>
+                        <input type="text" class="form-control" id="name" name="name" value="{{ $schema->name }}" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="station_id">Дэд станц</label>
+                        <select class="form-control" id="station_id" name="station_id" required>
+                            @foreach ($stations as $station)
+                                <option value="{{ $station->id }}" {{ $schema->station_id == $station->id ? 'selected' : '' }}>
+                                    {{ $station->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="image">Зураг</label>
+                        <input type="file" class="form-control mb-3" id="image" name="image">
+                        @if ($schema->image)
+                            <img src="{{ asset('storage/' . $schema->image) }}" width="100" height="100">
+                        @endif
+                    </div>
+                    <button type="submit" class="btn btn-primary">Хадгалах</button>
+                </form>
             </div>
-            <div class="form-group">
-                <label for="image">Зураг</label>
-                <input type="file" class="form-control-file" id="image" name="image">
-                @if ($schema->image)
-                    <img src="{{ asset('storage/' . $schema->image) }}" width="100" height="100">
-                @endif
-            </div>
-            <button type="submit" class="btn btn-primary">Хадгалах</button>
-        </form>
+        </div>
+        
     </div>
 @endsection
