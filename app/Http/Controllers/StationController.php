cb7402d49152a0a88f7e466e62332a6aaf517bf3
gantulgarus <gantulgarus@gmail.com>
2024-09-19 13:42:09 +0800

    update station

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -19,7 +19,7 @@ class StationController extends Controller
     {
         $stations = Station::with('branch', 'volts')->paginate(10);
         return view('stations.index', compact('stations'))
-            ->with('i', (request()->input('page', 1) - 1) * 5);
+            ->with('i', (request()->input('page', 1) - 1) * 10);
     }
 
     /**
